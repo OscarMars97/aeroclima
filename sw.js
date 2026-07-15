f2ac05e3a62c5a0fac8a704b71e9a4e4c3fc6a36
OscarMars97 <oscar12xd@gmail.com>
2026-07-15 16:43:23 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'aero-clima-v2';
+const APP_SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Solo cachea el "app shell" propio. Las llamadas a las APIs de clima
+// (open-meteo, etc.) siempre van a la red: no queremos servir pronósticos
+// viejos desde caché.
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  const isOwnOrigin = url.origin === self.location.origin;
+  if (!isOwnOrigin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((res) => {
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, res.clone()));
+          return res;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
